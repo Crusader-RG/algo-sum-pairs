b6b8cdae0a8e8153c8cd9f679c198955e3be8c26
Crusader-RG <justin.leeper92@gmail.com>
2024-05-31 20:04:16 -0600

    complete

--- python/sum_pairs.py
+++ python/sum_pairs.py
@@ -1,1 +1,14 @@
-def sum_pairs():
+def sum_pairs(arr, num):
+    ans_lst = []
+    for i in range(len(arr)):
+        for j in range(i + 1, len(arr)):
+            if arr[i] + arr[j] == num:
+                ans_lst.append([arr[i], arr[j]])
+                
+    if ans_lst == []:
+        return "unable to find pairs"
+    else:
+        return ans_lst
+    
+print(sum_pairs([3, 1, 5, 8, 2], 27))
+
