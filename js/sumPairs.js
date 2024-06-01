196f398f9edd4f17eb28a0a3b6cf7ac3e1c9bb4f
Crusader-RG <justin.leeper92@gmail.com>
2024-05-31 20:04:53 -0600

    complete

--- js/sumPairs.js
+++ js/sumPairs.js
@@ -1,3 +1,20 @@
-exports.sumPairs = function() {
-
+const sumPairs = (arr, num) => {
+    let ansArr = []
+    for (i = 0; i <= arr.length; i++){
+        for (j = i; j <= arr.length; j++){
+            if (i === j) {
+                continue
+            } else if (i + j === num){
+                ansArr.push(i)
+                ansArr.push(j)
+            }
+        }
+    }
+    if (ansArr.length === 0){
+        return "unable to find pairs"
+    } else {
+    return ansArr
+    }
 };
+
+module.exports = sumPairs;
